30691ff3e75f4600ced59179c27a9fc1e524845f
Justin Murdock <jmurdock@lyris.com>
2015-07-24 00:37:10 -0700

    save changes

---
+++ portfolio.php
@@ -0,0 +1,12 @@
+<?php
+    $justins_portfolio_links = array(        
+    array('image' => 'first image', 
+          'link' => 'first link') ,
+    array('image' => 'second image', 
+          'link' => 'second link')
+    );
+    
+    for($i = 0; $i < sizeof($justins_portfolio_links); $i++ ){
+        echo "<img src=\"{$justins_portfolio_links[$i]['image']}\" >\n";
+    }
+?>

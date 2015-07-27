5d77681c59ad2c10971411ac6d8fb5b95c4b7147
Justin Murdock <jmurdock@lyris.com>
2015-07-26 17:17:47 -0700

    links

---
+++ links.php
@@ -0,0 +1,43 @@
+<?php
+$links = array(
+    array( # $links[0]['title'] == 'Udacity Nanodegree - Frontend Web Development
+        'title' => "Udacity Nanodegree - Frontend Web Development",
+        'image' => "",
+        'link' => "http://http://juliusakula.github.io/certificates/Front%20End%20Web%20Developer%20Nanodegreee%20Completion.pdf"
+    ),array(
+        'title' => "Ruby on Rails - Web Application Architecture. University of New Mexico & Coursera *Distinction",
+        'image' => "",
+        'link' => "https://www.coursera.org/account/accomplishments/verify/7C3P4MSZZJ"
+    ),array(
+        'title' => "Code School - Javascript, HTML, Git",
+        'image' => "",
+        'link' => "https://www.codeschool.com/users/juliusakula"
+    ),# $links[2]['title'] == 'Code School - Javascript, HTML, Git
+    array(
+        'title' => "Boy VS Bugs - Javsascript Game, Demo on github",
+        'image' => "",
+        'link' => "http://juliusakula.github.io/game/"
+    ),array(
+        'title' => "Learn to Type faster in the browser, Demo on github.",
+        'image' => "",
+        'link' => "http://juliusakula.github.io/type-fast/"
+    ),array(
+        'title' => "Edit Markdown (Github documentation) in the browser, Demo on github",
+        'image' => "",
+        'link' => "http://juliusakula.github.io/editor/#/"
+    ), # $links[5]['title'] == 'Edit Markdown (Github documentation) in the browser, Demo on github
+    array(
+        'title' => "Hobby: Mining Bitcoin",
+        'image' => "http://i.imgur.com/yiD9YBM.jpg",
+        'link' => "http://www.juliusakula.com/abbreviated-litecoin-instructions/"
+    ),array(
+        'title' => "Hobby: StackOverflow Guru - Top 2% for total site-wide reputation, top tags include: AngularJS, PHP, Grunt, CSS, and SQL",
+        'image' => "",
+        'link' => "http://stackoverflow.com/users/3100494/j-dizzle?tab=profile"
+    ),array(
+        'title' => "Github hosted Resume and Contact Information",
+        'image' => "",
+        'link' => "http://juliusakula.github.io/resume/"
+    ) # $links[8]['title'] == 'Github hosted Resume and Contact Information
+); #/$links
+?>

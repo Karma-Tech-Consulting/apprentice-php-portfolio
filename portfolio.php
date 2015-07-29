8ecf52fbaf35cae8edf5cf18f5ace830c9f73b6c
Devon Warmack <devonwarmack@karmaconsulting.com>
2015-07-28 19:36:57 -0500

    completed 1st assignment

--- portfolio.php
+++ portfolio.php
@@ -1,20 +1,9 @@
 <?php
-    $justins_portfolio_links = array(        
-    array('image' => 'first image',
-          'link' => 'first link') ,
-    array('image' => 'second image',
-          'link' => 'second link'),
-    array('image' => 'third image',
-          'link' => 'third link')
-    );
-    
-    $J = 0;
-    echo "<img src=\"{$justins_portfolio_links[$J]['image']}\" >\n";
-    
     include 'links.php';
-    for($i = 0; $i < sizeof($justins_portfolio_links); $i++ ){
-        echo "<a href=\"{$justins_portfolio_links[$i]['link']}\" >";
-        echo "<img src=\"{$justins_portfolio_links[$i]['image']}\" >";
+    for($i = 0; $i < sizeof($links); $i++ ){
+        echo "<h1> {$links[$i]['title']} </h1>";
+        echo "<a href=\"{$links[$i]['link']}\" >";
+        echo "<img src=\"{$links[$i]['image']}\" >";
         echo "</a> \n";
     }
 ?>

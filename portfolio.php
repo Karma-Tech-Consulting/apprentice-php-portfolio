f2825f26a009958b5b4d29b5af10d13a91c98514
Justin Murdock <justin.murdock96@gmail.com>
2015-08-03 11:21:35 -0700

    add title to assignment challenge var

--- portfolio.php
+++ portfolio.php
@@ -1,15 +1,18 @@
 <?php
     $justins_portfolio_links = array(        
-    array('image' => 'first image',
+    array('title' => 'first Title',
+          'image' => 'first image',
           'link' => 'first link') ,
-    array('image' => 'second image',
+    array('title' => 'second Title',
+          'image' => 'second image',
           'link' => 'second link'),
-    array('image' => 'third image',
+    array('title' => 'third Title',
+          'image' => 'third image',
           'link' => 'third link')
     );
     
-    $J = 0;
-    echo "<img src=\"{$justins_portfolio_links[$J]['image']}\" >\n";
+    $J = 0; // testing this .. think about using I in the loop in the same way
+    echo "<h1>{$justins_portfolio_links[$J]['title']}</h1>";
     
     for($i = 0; $i < sizeof($justins_portfolio_links); $i++ ){
         echo "<img src=\"{$justins_portfolio_links[$i]['image']}\" >\n";
